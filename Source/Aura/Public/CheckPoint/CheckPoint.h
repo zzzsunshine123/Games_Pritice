e2f4fb4c60cf884fae1a273076039bfffec47c34
zzzsunshine123 <2638702058@qq.com>
2025-03-27 23:22:39 +0800

    savePlayerData

---
+++ Source/Aura/Public/CheckPoint/CheckPoint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "CheckPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ACheckPoint : public APlayerStart
+{
+	GENERATED_BODY()
+	
+};
